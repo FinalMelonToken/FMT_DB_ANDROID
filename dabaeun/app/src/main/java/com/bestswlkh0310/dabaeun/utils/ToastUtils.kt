00246a1de7b5ad6ed3dba771f8b4da3f28b225a3
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-08-19 13:39:07 +0900

    toast util

---
+++ dabaeun/app/src/main/java/com/bestswlkh0310/dabaeun/utils/ToastUtils.kt
@@ -0,0 +1,12 @@
+package com.bestswlkh0310.dabaeun.utils
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.longToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
